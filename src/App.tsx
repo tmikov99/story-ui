93f72cb64247ebc57819f98d3cb59d1554027bfb
Todor Mikov <tmikov99@gmail.com>
2025-02-13 23:32:52 +0200

    Add default header with default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import { useState } from 'react';
-import './App.css'
-
-import Navigation from './navigation/Navigation';
 import TextEditor from './textEditor/TextEditor';
+import Header from './components/Header';
+import AppTheme from './theme/AppTheme';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import { alpha } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+
 
 
 function App() {
 
   return (
-    <>
-      <Navigation></Navigation>
-      <TextEditor></TextEditor>
-    </>
+    <AppTheme>
+      <CssBaseline enableColorScheme />
+      <Box sx={{ display: 'flex' }}>
+        <Box 
+          component="main"
+          sx={(theme) => ({
+              flexGrow: 1,
+              backgroundColor: alpha(theme.palette.background.default, 1),
+              overflow: 'auto',
+            })}>
+          <Stack
+            spacing={2}
+            sx={{
+              alignItems: 'center',
+              mx: 3,
+              pb: 5,
+              mt: { xs: 8, md: 0 },
+            }}
+          >
+            <Header />
+            <TextEditor></TextEditor>
+          </Stack>
+        </Box>
+      </Box>
+
+    </AppTheme>
   )
 }
 
