219ae4b21b05538fd81ce7db5facc684a9a4c70a
Todor Mikov <61269302+tmikov99@users.noreply.github.com>
2025-03-14 01:59:56 +0200

    Add sign up and sign in (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -11,44 +11,53 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageCreateOverview from './components/PageCreateOverview';
 import PageCreate from './components/PageCreate';
 import PageCreateLinks from './components/PageCreateLinks';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 
-
+let signedIn = true;
 function App() {
-
   return (
     <AppTheme>
       <CssBaseline enableColorScheme />
       <BrowserRouter>
-        <Box sx={{ display: 'flex' }}>
-          <SideMenu />
-          <Box 
-            component="main"
-            sx={(theme) => ({
-                flexGrow: 1,
-                backgroundColor: alpha(theme.palette.background.default, 1),
-                overflow: 'auto',
-              })}>
-            <Stack
-              spacing={2}
-              sx={{
-                alignItems: 'center',
-                mx: 3,
-                pb: 5,
-                mt: { xs: 8, md: 0 },
-              }}
-            >
-              <Header />
-              <Routes>
-                <Route path="/" element={<MainGrid />}/>
-                <Route path="/page" element={<Page />}/>
-                <Route path="/create" element={<PageCreateOverview />}/>
-                <Route path="/createPage" element={<PageCreate />}/>
-                <Route path="createLinks" element={<PageCreateLinks />}/>
-              </Routes>    
-            </Stack>
+        {!signedIn ?
+          <Routes>
+            <Route path="signIn" element={<SignIn />} />
+            <Route path="signup" element={<SignUp />} />
+          </Routes> 
+          : 
+          <Box sx={{ display: 'flex' }}>
+            <SideMenu />
+            <Box 
+              component="main"
+              sx={(theme) => ({
+                  flexGrow: 1,
+                  backgroundColor: alpha(theme.palette.background.default, 1),
+                  overflow: 'auto',
+                })}>
+              <Stack
+                spacing={2}
+                sx={{
+                  alignItems: 'center',
+                  mx: 3,
+                  pb: 5,
+                  mt: { xs: 8, md: 0 },
+                }}
+              >
+                <Header />
+                <Routes>
+                  <Route path="/" element={<MainGrid />}/>
+                  <Route path="/page" element={<Page />}/>
+                  <Route path="/create" element={<PageCreateOverview />}/>
+                  <Route path="/createPage" element={<PageCreate />}/>
+                  <Route path="createLinks" element={<PageCreateLinks />}/>
+                </Routes>    
+              </Stack>
+            </Box>
           </Box>
-        </Box>
+        }
+        
       </BrowserRouter>
     </AppTheme>
   )
--- src/components/MenuContent.tsx
+++ src/components/MenuContent.tsx
@@ -6,7 +6,6 @@ import ListItemText from '@mui/material/ListItemText';
 import Stack from '@mui/material/Stack';
 // import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import AnalyticsRoundedIcon from '@mui/icons-material/AnalyticsRounded';
-import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
 import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 // import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
