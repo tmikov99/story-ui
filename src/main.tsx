5deda2e884ecf479be84b21b80b269d7908fd072
Todor Mikov <61269302+tmikov99@users.noreply.github.com>
2025-04-09 21:44:34 +0300

    Add redux and auth store (#23)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { StyledEngineProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
+    <Provider store={store}>
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
+    </Provider>
   </StrictMode>,
 )
