0c71bad9f673d7fe5d0d93878f3dc0311cd29dee
Todor Mikov <61269302+tmikov99@users.noreply.github.com>
2025-04-14 20:22:08 +0300

    Test refresh tokens (#30)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -10,6 +10,17 @@ const axiosInstance = axios.create({
   },
 });
 
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach(prom => {
+    if (error) prom.reject(error);
+    else prom.resolve(token);
+  });
+  failedQueue = [];
+};
+
 axiosInstance.interceptors.request.use((config) => {
   const token = sessionStorage.getItem('token');
   if (token) {
@@ -17,5 +28,49 @@ axiosInstance.interceptors.request.use((config) => {
   }
   return config;
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 403 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers["Authorization"] = `Bearer ${token}`;
+            return axiosInstance(originalRequest);
+          })
+          .catch((err) => Promise.reject(err));
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      try {
+        const res = await axiosInstance.get(
+          "/auth/refresh",
+        );
+        const newAccessToken = res.data.token;
+
+        sessionStorage.setItem("token", newAccessToken);
+        axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${newAccessToken}`;
+        processQueue(null, newAccessToken);
+
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        processQueue(err, null);
+        sessionStorage.removeItem("token");
+        window.location.href = "/login";
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
