0ce422f575e562da91bf6c1f27c171d44e39d572
Todor Mikov <61269302+tmikov99@users.noreply.github.com>
2025-04-21 12:03:16 +0300

    Add choice creation through graph (#35)

---
+++ src/utils/reactFlowUtil.tsx
@@ -0,0 +1,18 @@
+import { Handle, Position } from 'react-flow-renderer';
+import PageCard from '../components/PageCard';
+import { NodeTypes } from 'react-flow-renderer';
+import { PageData } from '../types/page';
+
+export function PageCardNode({ data }: { data: { page: PageData } }) {
+  return (
+    <div style={{ width: 300, position: "relative" }}>
+      <Handle type="target" position={Position.Left} style={{ background: "#555", width: "1rem", height: "1rem" }} />
+      <PageCard page={data.page} />
+      <Handle type="source" position={Position.Right} style={{ background: "#555", width: "1rem", height: "1rem" }} />
+    </div>
+  );
+}
+
+export const nodeTypes: NodeTypes = {
+  pageNode: PageCardNode,
+};
