089e3b468dad387ec5df2a40c179c48c47c4b0b5
Todor Mikov <tmikov99@gmail.com>
2025-04-24 21:46:09 +0300

    Add page card menu

--- src/utils/reactFlowUtil.tsx
+++ src/utils/reactFlowUtil.tsx
@@ -2,11 +2,21 @@ import { Handle, Position, NodeTypes } from '@xyflow/react';
 import PageCard from '../components/PageCard';
 import { PageData } from '../types/page';
 
-export function PageCardNode({ data, selected }: { data: { page: PageData }, selected: boolean }) {
+interface PageCardNodeProps {
+  data: { 
+    page: PageData;
+    onMenuOpen?: (e: React.MouseEvent<HTMLElement>, page: PageData) => void;
+   };
+  selected: boolean;
+  
+}
+
+export function PageCardNode({ data, selected }: PageCardNodeProps) {
+  const { page, onMenuOpen } = data;
   return (
     <div style={{ width: 300, position: "relative" }}>
       <Handle type="target" position={Position.Left} style={{ background: "#555", width: "1rem", height: "1rem" }} />
-      <PageCard page={data.page} selected={selected} />
+      <PageCard page={page} selected={selected} onMenuOpen={onMenuOpen}/>
       <Handle type="source" position={Position.Right} style={{ background: "#555", width: "1rem", height: "1rem" }} />
     </div>
   );
