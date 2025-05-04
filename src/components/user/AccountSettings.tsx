9b37896fa9f5d96f55031c60527687c3bb43509c
Todor Mikov <61269302+tmikov99@users.noreply.github.com>
2025-05-04 11:44:45 +0300

    Refactor some empty states (#57)

--- src/components/user/AccountSettings.tsx
+++ src/components/user/AccountSettings.tsx
@@ -6,7 +6,6 @@ import { getCurrentUser } from "../../api/user";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { User } from "../../types/user";
-import UserEmptyState from "../emptyState/UserEmptyState";
 
 export default function AccountSettings() {
   const username = useSelector((state: RootState) => state.auth.user?.username);
@@ -46,99 +45,94 @@ export default function AccountSettings() {
 
   return (
     <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
-      {username ? (
-        <Stack gap={2} sx={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-          <Typography component="h2" variant="h6" sx={{ width: '100%' }}>
-            Account Settings
-          </Typography>
-          <Box
+      <Stack gap={2} sx={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+        <Typography component="h2" variant="h6" sx={{ width: '100%' }}>
+          Account Settings
+        </Typography>
+        <Box
+          sx={{
+            position: 'relative',
+            width: 200,
+            height: 200,
+            cursor: 'pointer',
+            '&:hover .onHover': { display: 'flex' },
+            '&:hover .offHover': { display: 'none' },
+          }}
+        >
+          <Avatar 
+            src={user ? user.imageUrl : undefined}
+            alt={user ? user.username : undefined}
             sx={{
-              position: 'relative',
-              width: 200,
-              height: 200,
-              cursor: 'pointer',
-              '&:hover .onHover': { display: 'flex' },
-              '&:hover .offHover': { display: 'none' },
-            }}
-          >
-            <Avatar 
-              src={user ? user.imageUrl : undefined}
-              alt={user ? user.username : undefined}
+              width: '100%',   
+              height: '100%', 
+              fontSize: 100
+          }}>
+            <Typography className="offHover" sx={{fontSize: 100}}>{user?.username[0]}</Typography>
+          </Avatar>
+          {uploading && (
+            <Box
               sx={{
-                width: '100%',   
-                height: '100%', 
-                fontSize: 100
-            }}>
-              <Typography className="offHover" sx={{fontSize: 100}}>{user?.username[0]}</Typography>
-            </Avatar>
-            {uploading && (
-              <Box
-                sx={{
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  width: '100%',
-                  height: '100%',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  bgcolor: 'rgba(255, 255, 255, 0.6)',
-                  borderRadius: '50%',
-                }}
-              >
-                <CircularProgress size={64} />
-              </Box>
-            )}
-            <IconButton 
-              className="onHover"
-              sx={{
-                display: 'none',
                 position: 'absolute',
                 top: 0,
                 left: 0,
                 width: '100%',
                 height: '100%',
-                bgcolor: 'rgba(0, 0, 0, 0.4)',
-                color: 'white',
-                justifyContent: 'center',
+                display: 'flex',
                 alignItems: 'center',
-                fontSize: 100,
+                justifyContent: 'center',
+                bgcolor: 'rgba(255, 255, 255, 0.6)',
+                borderRadius: '50%',
               }}
-              disableRipple
-              aria-label="upload photo"
-              component="label"
             >
-              <AddAPhotoRoundedIcon  sx={{fontSize: 100}} />
-              <input
-                type="file"
-                hidden
-                accept="image/*"
-                onChange={handleFileChange}
-              />
-            </IconButton>
-          </Box>
-          
-          <Typography variant="h2">{user?.username}</Typography>
-          <Typography>{user?.email}</Typography>
-          <Typography component="h2" variant="h6" sx={{ width: '100%' }}>
-            Password Settings
-          </Typography>
-          {changingPassword ? 
-            <>
-              <TextField type="password" label="New Password"/>
-              <TextField type="password" label="Confirm New Password" />
-              <Button variant="contained" onClick={() => setChangingPassword(false)}>Confirm</Button>
-            </> :
-            <Button variant="contained" onClick={() => setChangingPassword(true)}>Change Password</Button>
-          }
-        </Stack>
-      ) : (
-        <UserEmptyState />
-      )
-    }
+              <CircularProgress size={64} />
+            </Box>
+          )}
+          <IconButton 
+            className="onHover"
+            sx={{
+              display: 'none',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              bgcolor: 'rgba(0, 0, 0, 0.4)',
+              color: 'white',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontSize: 100,
+            }}
+            disableRipple
+            aria-label="upload photo"
+            component="label"
+          >
+            <AddAPhotoRoundedIcon  sx={{fontSize: 100}} />
+            <input
+              type="file"
+              hidden
+              accept="image/*"
+              onChange={handleFileChange}
+            />
+          </IconButton>
+        </Box>
+        
+        <Typography variant="h2">{user?.username}</Typography>
+        <Typography>{user?.email}</Typography>
+        <Typography component="h2" variant="h6" sx={{ width: '100%' }}>
+          Password Settings
+        </Typography>
+        {changingPassword ? 
+          <>
+            <TextField type="password" label="New Password"/>
+            <TextField type="password" label="Confirm New Password" />
+            <Button variant="contained" onClick={() => setChangingPassword(false)}>Confirm</Button>
+          </> :
+          <Button variant="contained" onClick={() => setChangingPassword(true)}>Change Password</Button>
+        }
+      </Stack>
     </Box>
   );
 }
