c6628f5a00fe4dfade3b8ea258985717461db0ce
Todor Mikov <61269302+tmikov99@users.noreply.github.com>
2025-05-20 22:26:03 +0300

    Skip auth header population when not needed (#103)

--- src/api/images.ts
+++ src/api/images.ts
@@ -1,14 +1,5 @@
 import axios from "./axios";
 
-export const saveThumbnail = async (formData: FormData) => {
-  const response = await axios.post(`/files/upload-thumbnail`, formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
-  return response.data;
-}
-
 export const saveUserPicture = async (formData: FormData) => {
   const response = await axios.post("/user/picture", formData, {
     headers: {
